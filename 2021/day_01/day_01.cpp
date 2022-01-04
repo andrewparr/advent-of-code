f486bd3f63651d9ecd5377ffc5ce134350cfc9d7
andrewparr <10243811+andrewparr@users.noreply.github.com>
2022-01-04 17:38:56 +0000

    day 1

---
+++ 2021/day_01/day_01.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, char** argv) {
+
+    std::vector<int> input;
+    while (!std::cin.eof()) {
+        int value;
+        std::cin >> value;
+        input.push_back(value);
+    }
+
+    int part_1 = 0;
+    int part_2 = 0;
+    for (size_t i = 1; i < input.size(); ++i) {
+        if (input[i] > input[i-1])
+            part_1++;
+        if (i > 3 && input[i] > input[i-3])
+            part_2++;
+    }
+
+    std::cout << part_1 << std::endl;
+    std::cout << part_2 << std::endl;
+
+    return 0;
+}
